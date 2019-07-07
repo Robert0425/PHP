b30618493a8156ccfc0c05bdd5a4e769cc2cd469
Robert0425 <45540576+Robert0425@users.noreply.github.com>
2019-07-07 11:07:33 +0800

    Create login.php

---
+++ login.php
@@ -0,0 +1,10 @@
+<form method="post" action="checkAccount.php">
+    Account: <input type="text" name="account"><br>
+    Password: <input type="password" name="passwd" /><br>
+    <input type="text" name="imgCode">
+    <img src="createImage.php" alt=""><br>
+    <input type="submit" value="Login" /><br>
+    <button type="button" onclick="location.href='register.php'">
+    Register
+    </button>
+</form>
